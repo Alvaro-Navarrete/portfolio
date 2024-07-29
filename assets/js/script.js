78b3552b7c35b2930979bafa4b11de5a4a2d4fb1
Alvaro Navarrete <115111730+Alvaro-Navarrete@users.noreply.github.com>
2024-07-29 00:03:49 -0400

    commit portfolio

---
+++ assets/js/script.js
@@ -0,0 +1,85 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const inicio = document.getElementById('inicio');
+    const proyecto = document.getElementById('proyecto');
+    const habilidad = document.getElementById('habilidad');
+    const sobreMi = document.getElementById('sobre-mi');
+    const contacto = document.getElementById('contacto');
+
+
+
+    $(function () {
+
+        $(window).on("scroll", function () {
+
+            const elementLeft = document.querySelectorAll('.info-izquierda');
+            const elementRight = document.querySelectorAll('.info-derecha');
+            var altura = window.innerHeight / 1.5;
+
+
+            elementLeft.forEach((element, i) => {
+                var distancia = element.getBoundingClientRect().top;
+
+                if (distancia <= altura) {
+
+                    element.classList.add('visible');
+                } else {
+                    element.classList.remove('visible');
+                }
+            })
+
+            elementRight.forEach((element, i) => {
+                const distancia = element.getBoundingClientRect().top;
+
+                if (distancia <= altura) {
+                    element.classList.add('visible');
+                } else {
+                    element.classList.remove('visible');
+                }
+            });
+
+        })
+
+       
+        $(inicio).on("click", function(event){
+            var posicion = $("body").offset().top
+
+            $('html, body').animate({
+                scrollTop: posicion
+            }, 100);
+        });
+
+        $(proyecto).on("click", function(event){
+            var posicion = $("#project-container").offset().top
+
+            $('html, body').animate({
+                scrollTop: posicion
+            }, 100);
+        });
+        $(habilidad).on("click", function(event){
+            var posicion = $("#skills").offset().top
+
+            $('html, body').animate({
+                scrollTop: posicion
+            }, 100);
+        });
+        $(sobreMi).on("click", function(event){
+            var posicion = $("#about-me").offset().top
+
+            $('html, body').animate({
+                scrollTop: posicion
+            }, 100);
+        });
+        $(contacto).on("click", function(event){
+            var posicion = $("#contact").offset().top
+
+            $('html, body').animate({
+                scrollTop: posicion
+            }, 100);
+        });
+
+
+
+    })
+
+
+})
